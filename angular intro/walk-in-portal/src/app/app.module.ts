70534bcb0b2accb362e1278944d5c6785599da6e
Prince <prince.sheth@zeuslearning.com>
2023-01-30 18:28:40 +0530

    added register

--- angular intro/walk-in-portal/src/app/app.module.ts
+++ angular intro/walk-in-portal/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
