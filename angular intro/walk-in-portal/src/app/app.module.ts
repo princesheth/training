1894c0a56535b375bf6baf63d0e1f2e07a869ea3
Prince <prince.sheth@zeuslearning.com>
2023-02-01 17:28:32 +0530

    partial ceartion of page one

--- angular intro/walk-in-portal/src/app/app.module.ts
+++ angular intro/walk-in-portal/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms'
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterComponent } from './register/register.component';
-import { FirstStepComponent } from './register/first-step/first-step.component';
+import { HeaderComponent } from './header/header.component';
+import { RHeaderComponent } from './register/r-header/r-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
     RegisterComponent,
-    FirstStepComponent,
+    HeaderComponent,
+    RHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatStepperModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular intro/walk-in-portal/src/app/register/register.component.ts
+++ angular intro/walk-in-portal/src/app/register/register.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterLink,Router } from '@angular/router';
+import {FormBuilder, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {showError: true},
+    },
+  ],
 })
 export class RegisterComponent {
-
+  firstFormGroup = this._formBuilder.group({
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    email: ['', Validators.required],
+    phone: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
+  thirdFormGroup = this._formBuilder.group({
+  });
+  constructor(private _formBuilder: FormBuilder) {}
 }
