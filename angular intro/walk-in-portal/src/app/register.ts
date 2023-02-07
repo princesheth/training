b40427a9af3b2f72714eb300b3f8d10af85d4ebd
Prince <prince.sheth@zeuslearning.com>
2023-02-07 15:16:49 +0530

    completed user registration

--- angular intro/walk-in-portal/src/app/register.ts
+++ angular intro/walk-in-portal/src/app/register.ts
@@ -1,12 +1,44 @@
 export class Personal_info{
-    firstname: string;
-    lastname: string;
-    email: string;
-    phoneno: string;
-    portfolio: string;
-    preferedjob: string[];
-    referer:string;
-    emailupadtes:boolean;
-    cv:string;
-    photo:string;
+    firstname: string="";
+    lastname: string="";
+    email: string="";
+    phoneno: string="";
+    portfolio: string="";
+    preferedjob: string[]=[];
+    referer:string="";
+    emailupdates:boolean;
+    cv:string="";
+    photo:string="";
+}
+
+export class edu_qualification{
+    percentage:number;
+    yop:number;
+    Qualification:string="";
+    stream:string="";
+    college:string="";
+    other_college:string="";
+    college_location:string="";
+    applicant_type:string="";
+}
+
+export class fresher_qualification{
+    techs:string[]=[];
+    other_tech:string="";
+    role:string="";
+    appeared:string="";
+}
+export class experienced_qualification{
+    experience:string="";
+    c_ctc:string="";
+    e_ctc:string="";
+    techs:string[]=[];
+    techs_e:string[]=[];
+    other_tech:string="";
+    other_tech_e:string="";
+    notice:string="";
+    notice_end: string="";
+    notice_length:string="";
+    appeared:string="";
+    role:string="";
 }
--- angular intro/walk-in-portal/src/app/register/register.component.ts
+++ angular intro/walk-in-portal/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Personal_info } from '../register';
+import { edu_qualification, experienced_qualification, fresher_qualification, Personal_info } from '../register';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +12,10 @@ import { Personal_info } from '../register';
 
 export class RegisterComponent {
   img:string="../assets/img/Zeus-LMS-logo.svg";
-  personal_info: Personal_info;
+  personal_info: Personal_info=new Personal_info();
+  e_quali:edu_qualification=new edu_qualification();
+  fresher_q:fresher_qualification=new fresher_qualification();
+  experienced_q:experienced_qualification=new experienced_qualification();
   months: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   panelOpenState = false;
 
@@ -36,22 +39,37 @@ export class RegisterComponent {
   secondFormGroup = this._formBuilder.group({
     percentage: ['', Validators.required],
     YOP: ['', Validators.required],
+    qualification:['',Validators.required],
+    stream:['',Validators.required],
+    college:[''],
+    other_college:[''],
     c_location: ['', Validators.required],
+    type: ['', Validators.required],
+    other_tech: [''],
+    appeared:['',Validators.required],
+    role: [''],
     experience: ['', Validators.required],
     c_CTC: ['', Validators.required],
     e_CTC: ['', Validators.required],
-    other: [''],
-    other_tech: [''],
-    role: [''],
+    other_tech_e:[''],
+    notice:['',Validators.required],
+    notice_end:['',Validators.required],
+    notice_length:['',Validators.required],
     Javascript: false,
     Angular_JS: false,
     React: false,
     Node_JS: false,
     Others: false,
+    Javascript_e: false,
+    Angular_JS_e: false,
+    React_e: false,
+    Node_JS_e: false,
+    Others_e: false,
   });
 
 
   thirdFormGroup = this._formBuilder.group({});
+  fill: boolean=false;
 
 
   constructor(private _formBuilder: FormBuilder) {}
@@ -64,6 +82,19 @@ export class RegisterComponent {
 
   Change(e: any) {
     this.personal = e.value;
+    this.secondFormGroup.patchValue({
+    Javascript : false,
+    Javascript_e : false,
+    React : false,
+    React_e : false,
+    Node_JS : false,
+    Node_JS_e : false,
+    Angular_JS : false,
+    Angular_JS_e : false,
+    Others : false,
+    Others_e : false
+    })
+    
   }
 
 
@@ -86,8 +117,7 @@ export class RegisterComponent {
 
   cv_name:string;
   cv_upload(e: any) {
-    console.log(e)
-    // this.cv_name=e.target.files[0].name;
+    this.cv_name=e.target.files[0].name;
   }
 
   photo:string;
@@ -96,7 +126,7 @@ export class RegisterComponent {
   }
 
 
-  preferedjob: string[] = [];
+  preferedjob: string[]=[];
 
 
   form1() {
@@ -117,16 +147,94 @@ export class RegisterComponent {
       referer: this.firstFormGroup.value.referal as string,
       portfolio: this.firstFormGroup.value.portfolio as string,
       preferedjob: this.preferedjob,
-      emailupadtes: this.firstFormGroup.value.update as boolean,
+      emailupdates: this.firstFormGroup.value.update as boolean,
       cv:this.cv_name,
       photo:this.photo
     };
+    console.log(this.personal_info)
   }
 
-
+  tech:string[]=[];
+  tech_e:string[]=[];
   form2(){
+    this.e_quali={
+      percentage:+this.secondFormGroup.value.percentage!,
+      yop:+this.secondFormGroup.value.YOP!,
+      Qualification:this.secondFormGroup.value.qualification as string,
+      stream:this.secondFormGroup.value.stream as string,
+      college:this.secondFormGroup.value.college as string,
+      other_college:this.secondFormGroup.value.other_college as string,
+      college_location:this.secondFormGroup.value.c_location as string,
+      applicant_type:this.secondFormGroup.value.type as string
+    }
+
+    if(this.secondFormGroup.value.type == 'Fresher'){
+      if(this.secondFormGroup.value.Javascript){
+        this.tech.push('Javascript')
+      }
+      if(this.secondFormGroup.value.Angular_JS){
+        this.tech.push('Angular JS')
+      }
+      if(this.secondFormGroup.value.Node_JS){
+        this.tech.push('Node JS')
+      }
+      if(this.secondFormGroup.value.React){
+        this.tech.push('React')
+      }
+      this.fresher_q={
+        techs:this.tech,
+        other_tech:this.secondFormGroup.value.other_tech as string,
+        role:this.secondFormGroup.value.role as string,
+        appeared:this.secondFormGroup.value.appeared as string
+      }
+      this.experienced_q=new experienced_qualification();
+    }
+    else if(this.secondFormGroup.value.type == 'Experienced'){
+      if(this.secondFormGroup.value.Javascript){
+        this.tech.push('Javascript')
+      }
+      if(this.secondFormGroup.value.Angular_JS){
+        this.tech.push('Angular JS')
+      }
+      if(this.secondFormGroup.value.Node_JS){
+        this.tech.push('Node JS')
+      }
+      if(this.secondFormGroup.value.React){
+        this.tech.push('React')
+      }
+      if(this.secondFormGroup.value.Javascript_e){
+        this.tech_e.push('Javascript')
+      }
+      if(this.secondFormGroup.value.Angular_JS_e){
+        this.tech_e.push('Angular JS')
+      }
+      if(this.secondFormGroup.value.Node_JS_e){
+        this.tech_e.push('Node JS')
+      }
+      if(this.secondFormGroup.value.React_e){
+        this.tech_e.push('React')
+      }
+      this.experienced_q={
+        experience:this.secondFormGroup.value.experience as string,
+        c_ctc:this.secondFormGroup.value.c_CTC as string,
+        e_ctc:this.secondFormGroup.value.e_CTC as string,
+        techs:this.tech,
+        techs_e:this.tech_e,
+        other_tech:this.secondFormGroup.value.other_tech as string,
+        other_tech_e:this.secondFormGroup.value.other_tech_e as string,
+        notice_end:this.secondFormGroup.value.notice_end! as string,
+        notice_length:this.secondFormGroup.value.notice_length as string,
+        role:this.secondFormGroup.value.role as string,
+        notice:this.secondFormGroup.value.notice as string,
+        appeared:this.secondFormGroup.value.appeared as string
+      }
+      this.fresher_q=new fresher_qualification();
+    }
+
+    console.log(this.e_quali,this.experienced_q,this.fresher_q)
+    this.fill=true;
+
     var dis = document.querySelector("#disabled");
-    console.log(dis)
     dis!.classList.remove('disabled');
   }
 }
